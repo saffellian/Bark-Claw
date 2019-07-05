46c98c164b2b9319b4c0ad9e218fd68225af13be
Ethan <e_q101@live.com>
2019-07-05 11:44:30 -0500

    Fixed meta files not being included

---
+++ Assets/AnimationDelay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationDelay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int startDelay;
+    Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(DelayedAnimation());
+    }
+
+    // The delay coroutine
+    IEnumerator DelayedAnimation()
+    {
+        yield return new WaitForSeconds(startDelay);
+        animator.Play("DOg-Strumming_0");
+    }
+
+}
