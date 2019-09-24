936e1266d2516f4eec24b46b3c8fcee169792a4d
Mike <mjshl2@hotmail.com>
2019-09-24 09:16:06 -0700

    Door animation, Logo Fix, House changes

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("OpenDoor");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        anim.enabled = true;
+    }
+
+    void PauseAnimationEvent()
+    {
+        anim.enabled = false;
+    }
+
+}
+
+
