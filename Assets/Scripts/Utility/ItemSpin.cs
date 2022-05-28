75d07ef3aacbb0aa33d3bf9af671f1adac9050f8
Ethan Grahn <e_q101@live.com>
2022-05-28 16:39:31 -0400

    Prefab and animation edits

--- Assets/Scripts/Utility/ItemSpin.cs
+++ Assets/Scripts/Utility/ItemSpin.cs
@@ -10,14 +10,29 @@ public class ItemSpin : MonoBehaviour
     }
 
     [SerializeField] private AnimationCurve BobCurve;
+    [SerializeField] private bool randomizeBobOffset = false;
     [SerializeField] private float bobSpeed = 1f;
+    [SerializeField] private bool randomizeRotateSpeed = true;
     [SerializeField] private float rotateSpeed = 20f;
     [SerializeField] private Axis rotateAxis = Axis.Z;
 
     private float startY;
     private Vector3 axis;
+    private float bobOffset = 0f;
+    private float rotateOffset = 0f;
 
     private void Start() {
+        if (randomizeBobOffset)
+        {
+            bobOffset = Random.Range(0f, 0.99f);
+        }
+
+        if (randomizeRotateSpeed)
+        {
+            float qSpeed = (rotateSpeed / 4f);
+            rotateOffset = Random.Range(rotateSpeed - qSpeed, rotateSpeed + qSpeed);
+        }
+
         startY = transform.position.y;
         switch(rotateAxis)
         {
@@ -33,10 +48,9 @@ public class ItemSpin : MonoBehaviour
         }  
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, startY + BobCurve.Evaluate(Time.fixedTime * bobSpeed), transform.position.z);
+        transform.position = new Vector3(transform.position.x, startY + BobCurve.Evaluate((Time.fixedTime + bobOffset) * bobSpeed), transform.position.z);
 
         transform.Rotate(axis, Time.deltaTime * rotateSpeed);
     }
---
+++ Assets/Scripts/Utility/KillBoundary2D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class KillBoundary2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.transform.CompareTag("Player"))
+        {
+            return;
+        }
+
+        other.GetComponent<PlayerHealth>().InstantDeath();
+    }
+}
